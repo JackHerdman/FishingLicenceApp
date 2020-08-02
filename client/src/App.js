54a82693ecf48d417cf3f083bf682ed353369b4c
Jack Herdman <jack.herdman3@studytafensw.edu.au>
2020-08-02 20:32:45 +1000

    corrects error on landing page

--- client/src/App.js
+++ client/src/App.js
@@ -21,45 +21,28 @@ export default function App() {
   }, []);
 
   return (
-    <div>
-      <div class="pimg1">
-        <div class="ptext">
-          <span class="myborder">
-            NSW Fishing Licence Management
-          </span>
-        </div>
-      </div>
 
-      <section class="section">
-        <h1>This is my Section</h1>
-      </section>
-
-    </div>
-
-    // <Router>
-    //   <Container className="p-3 my-3 border  ml-auto">
-    //     <Jumbotron>
-    //       <Row>
-    //         <Col sm={4}>
-    //           <Image className="" src="./images/fishing.jpg" />
-    //         </Col>
-    //         <Col sm={8}>
-    //           <h1>NSW Fishing Licence Management</h1>
-    //         </Col>
-    //       </Row>
-    //     </Jumbotron>
-    //     <Switch>
-    //       <Route exact path="/">
-    //         <ListLicences licences={licences} />
-    //       </Route>
-    //       <Route path="/create-new">
-    //         <CreateNew />
-    //       </Route>
-    //       <Route path="/delete/:id">
-    //         <Delete />
-    //       </Route>
-    //     </Switch>
-    //   </Container>
-    // </Router>
+    <Router>
+      <Container className="p-3 my-3 border  ml-auto">
+        <Jumbotron>
+          <Row>
+            <Col sm={8}>
+              <h1>NSW Fishing Licence Management</h1>
+            </Col>
+          </Row>
+        </Jumbotron>
+        <Switch>
+          <Route exact path="/">
+            <ListLicences licences={licences} />
+          </Route>
+          <Route path="/create-new">
+            <CreateNew />
+          </Route>
+          <Route path="/delete/:id">
+            <Delete />
+          </Route>
+        </Switch>
+      </Container>
+    </Router>
   );
 }
